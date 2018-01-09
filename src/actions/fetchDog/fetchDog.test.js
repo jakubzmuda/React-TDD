d71fdff199f782aa1466e726f627107673614360
Jakub Żmuda <goodguykuba@gmail.com>
2018-01-09 14:05:15 +0100

    switched to jest-axios-mock

--- src/actions/fetchDog/fetchDog.test.js
+++ src/actions/fetchDog/fetchDog.test.js
@@ -1,41 +1,38 @@
-import sinon from 'sinon';
+import mockAxios from 'jest-mock-axios';
 import configureMockStore from 'redux-mock-store';
 import { FETCH_DOG_REQUEST, FETCH_DOG_SUCCESS } from '../../constants/actionTypes';
 import fetchDog from './fetchDog';
 
 describe('fetchDog action', () => {
-  let server;
+
   let store;
 
   beforeEach(() => {
-    server = sinon.fakeServer.create();
-    server.autoRespond = true;
-
     const mockStore = configureMockStore();
     store = mockStore({});
   });
 
   afterEach(() => {
-    server.restore();
+    mockAxios.reset();
   });
 
-  it('fetches a dog', async () => {
+  it('fetches a dog', () => {
     // given
-    server.respondWith('GET', `https://dog.ceo/api/breeds/image/random`,
-      [
-        200,
-        { 'Content-Type': 'application/json' },
-        '{"status":"success","message":"https://dog.ceo/api/img/someDog.jpg"}'
-      ]);
+    const dogApiResponse = {
+      status: 'success',
+      message: 'https://dog.ceo/api/img/someDog.jpg',
+    };
     // when
-    await fetchDog()(store.dispatch);
+    fetchDog()(store.dispatch);
+    mockAxios.mockResponse({
+      data: dogApiResponse
+    });
     // then
-    expect(server.requests.length).toBe(1);
-    expect(server.requests[0].url).toBe('https://dog.ceo/api/breeds/image/random');
+    expect(mockAxios.get).toHaveBeenCalledWith('https://dog.ceo/api/breeds/image/random');
     expect(store.getActions().map(p => p.type)).toEqual(
       expect.arrayContaining([
         FETCH_DOG_REQUEST,
         FETCH_DOG_SUCCESS,
       ]));
-  });
+  })
 });
--- src/actions/fetchDog/fetchDogAlternative.test.js
+++ src/actions/fetchDog/fetchDogAlternative.test.js
@@ -1,38 +1,43 @@
-import axios from 'axios';
+import sinon from 'sinon';
 import configureMockStore from 'redux-mock-store';
 import { FETCH_DOG_REQUEST, FETCH_DOG_SUCCESS } from '../../constants/actionTypes';
 import fetchDog from './fetchDog';
 
-jest.mock('axios');
+jest.unmock('axios');
 
 describe('fetchDog action', () => {
-
+  let server;
   let store;
 
   beforeEach(() => {
+    server = sinon.fakeServer.create();
+    server.autoRespond = true;
+
     const mockStore = configureMockStore();
     store = mockStore({});
   });
 
+  afterEach(() => {
+    server.restore();
+  });
+
   it('fetches a dog', async () => {
     // given
-    const response = Promise.resolve({
-      data: {
-        status: 'success',
-        message: 'https://dog.ceo/api/img/someDog.jpg',
-      },
-      status: 200,
-    });
-    const requestMock = jest.fn().mockReturnValueOnce(response);
-    axios.get.mockImplementation(requestMock);
+    server.respondWith('GET', `https://dog.ceo/api/breeds/image/random`,
+      [
+        200,
+        { 'Content-Type': 'application/json' },
+        '{"status":"success","message":"https://dog.ceo/`api/img/someDog.jpg"}'
+      ]);
     // when
     await fetchDog()(store.dispatch);
     // then
-    expect(requestMock.mock.calls.length).toBe(1);
+    expect(server.requests.length).toBe(1);
+    expect(server.requests[0].url).toBe('https://dog.ceo/api/breeds/image/random');
     expect(store.getActions().map(p => p.type)).toEqual(
       expect.arrayContaining([
         FETCH_DOG_REQUEST,
         FETCH_DOG_SUCCESS,
       ]));
-  })
+  });
 });
